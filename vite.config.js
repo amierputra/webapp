bad4cd2326785bcb1ec18f0862727ab61320f007
Amier Putra <86624398+amierputra@users.noreply.github.com>
2023-10-06 20:39:03 +0800

    add hot reload for vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+
+import fs from 'fs';
+
+const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
     plugins: [
@@ -8,4 +12,22 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '$': 'jQuery'
+        },
+    },
+    server: {
+        host: true,
+        port: env.VITE_ASSET_PORT,
+        strictPort: true,
+        hmr: {
+            host: env.VITE_ASSET_HOST,
+            port: env.VITE_ASSET_PORT,
+        },
+        https: {
+            key: fs.readFileSync(env.VITE_PRIVKEY_PATH),
+            cert: fs.readFileSync(env.VITE_CERT_PATH),
+        },
+    }, 
 });
