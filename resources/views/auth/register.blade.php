7c9c3b35fb8f2d674c28cf279802d1b35e63bf33
Amier Putra <86624398+amierputra@users.noreply.github.com>
2023-10-08 20:33:55 +0800

    Create register function

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,38 @@
+<x-layouts.guest>
+    <div class="bg-white w-1/4 p-8 rounded-md">
+        <div class="flex justify-center items-center">
+            <h1 class="text-2xl font-bold uppercase mb-6">Register</h1>
+        </div>
+        <form action="{{ route('register.store') }}" method="POST">
+            @csrf
+            <div class="flex flex-col space-y-1 mb-4">
+                <label for="name" class="">Name</label>
+                <input type="text" name="name" id="name" class="rounded-md border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                @if ($errors->has('name'))
+                <span class="text-danger">{{ $errors->first('name') }}</span>
+                @endif
+            </div>
+            <div class="flex flex-col space-y-1 mb-4">
+                <label for="email" class="">Email</label>
+                <input type="email" name="email" id="email" class="rounded-md border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                @if ($errors->has('email'))
+                <span class="text-danger">{{ $errors->first('email') }}</span>
+                @endif
+            </div>
+            <div class="flex flex-col space-y-1 mb-4">
+                <label for="password" class="">Password</label>
+                <input type="password" name="password" id="password" class="rounded-md border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                @if ($errors->has('password'))
+                <span class="text-danger">{{ $errors->first('password') }}</span>
+                @endif
+            </div>
+            <div class="flex flex-col space-y-1 mb-4">
+                <label for="password_confirmation" class="">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="rounded-md border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+            </div>
+            <div class="w-full">
+                <button type="submit" class="w-full bg-gray-700 hover:bg-black text-white rounded-md p-2 mt-4">Register</button>
+            </div>
+        </form>
+    </div>
+</x-layouts.guest>
