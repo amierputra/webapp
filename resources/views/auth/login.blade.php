038889bfada35cf2823173691ff794e6215487d2
Amier Putra <86624398+amierputra@users.noreply.github.com>
2023-10-13 17:42:08 +0800

    add form error message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.guest>
-    <div class="bg-white w-1/4 p-8 rounded-md">
+    <div class="bg-white w-1/4 p-8 rounded-lg">
         <div class="flex justify-center items-center">
             <h1 class="text-2xl font-bold uppercase mb-6">Login</h1>
         </div>
@@ -7,14 +7,20 @@
             @csrf
             <div class="flex flex-col space-y-1 mb-4">
                 <label for="email" class="">Email</label>
-                <input type="email" name="email" id="email" class="rounded-md border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                <input type="email" name="email" id="email" class="rounded-lg border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                @if ($errors->has('email'))
+                <span class="text-red-600">{{ $errors->first('email') }}</span>
+                @endif
             </div>
             <div class="flex flex-col space-y-1 mb-4">
                 <label for="password" class="">Password</label>
-                <input type="password" name="password" id="password" class="rounded-md border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                <input type="password" name="password" id="password" class="rounded-lg border border-blue-800 focus:outline-none focus:border-sky-500 bg-gray-100 p-2">
+                @if ($errors->has('password'))
+                <span class="text-red-600">{{ $errors->first('password') }}</span>
+                @endif
             </div>
         <div class="w-full">
-            <button type="submit" class="w-full bg-blue-900 hover:bg-black text-white rounded-md p-2 mt-4">Login</button>
+            <button type="submit" class="w-full bg-blue-900 hover:bg-black text-white rounded-lg p-2 mt-4">Login</button>
         </div>
         </form>
     </div>
