adb14519ff7bf6f927f563410e1aec6906aaae51
Amier Putra <86624398+amierputra@users.noreply.github.com>
2023-10-15 03:34:14 +0800

    add ajax for update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.app>
+    <div id="successMessageContainer"></div>
     @if (session()->has('success'))
     <div class="flex items-center p-4 mb-4 text-sm bg-green-500 text-green-50 rounded-lg" role="alert">
         <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -77,8 +78,10 @@
                         <h2 class="text-xl font-bold mb-4 uppercase">Edit User</h2>
                         <div class="flex justify-center items-center">
                             <div class="w-full">
-                                <form action="" method="post">
+                                <form action="{{ route('admin.users.update', ['id' => ':id']) }}" method="post" id="editUserForm">
                                     @csrf
+                                    @method('PUT')
+                                    <input type="hidden" name="user_id" id="user_id">
                                     <div class="flex flex-col space-y-1 mb-4">
                                         <label for="name" class="">Name</label>
                                         <input type="text" name="name" id="name" class="rounded-lg border border-violet-800 focus:outline-none focus:ring focus:ring-violet-900 focus:ring-offset-1 bg-gray-100 p-2">
@@ -98,7 +101,7 @@
                                             <button type="button" class="text-black font-semibold bg-white hover:bg-gray-200 border border-violet-800 rounded-lg px-4 py-2 mt-4 w-full shadow-sm" id="closeFormEdit">Cancel</button>
                                         </div>
                                         <div class="w-full">
-                                            <button type="submit" class="text-white font-semibold bg-violet-800 hover:bg-violet-950 border border-violet-800 rounded-lg px-4 py-2 mt-4 w-full">Add User</button>
+                                            <button type="button" class="text-white font-semibold bg-violet-800 hover:bg-violet-950 border border-violet-800 rounded-lg px-4 py-2 mt-4 w-full" id="updateUserButton">Update User</button>
                                         </div>
                                     </div>
                                 </form>
@@ -119,20 +122,24 @@
                     <thead>
                         <tr class="bg-gray-800 text-white text-left">
                             <th class="p-4">No.</th>
-                            <th class="p-4">Email</th>
                             <th class="p-4">Name</th>
+                            <th class="p-4">Role</th>
+                            <th class="p-4">Email</th>
                             <th class="p-4">Status</th>
                             <th class="p-4">Action</th>
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-slate-200">
                         @foreach($users as $user)
-                        <tr class="">
+                        <tr class="clickable-row cursor-pointer hover:bg-gray-200" data-href="{{ route('admin.users.show', $user) }}">
                             <td class="px-4 py-2">{{ $loop->iteration }}</td>
-                            <td class="px-4 py-2">{{ $user->email }}</td>
-                            <td class="px-4 py-2">{{ $user->name }}</td>
+                            <td class="px-4 py-2" id="user-name">{{ $user->name }}</td>
+                            <td class="px-4 py-2 text-sm text-gray-500">
+                                Admin
+                            </td>
+                            <td class="px-4 py-2 text-sm text-gray-500" id="user-email">{{ $user->email }}</td>
                             <td class="px-4 py-2">
-                                <p class="inline-block bg-green-300 text-green-800 text-xs font-bold rounded-full px-4 py-1">Active</p>
+                                <p class="inline-block bg-green-100 border border-green-200 text-green-800 text-xs font-bold rounded-lg px-2 py-1">Active</p>
                             </td>
                             <td class="flex flex-row px-4 py-2 space-x-2">
                                 <a href="{{ route('admin.users.show', $user) }}">
@@ -159,6 +166,14 @@
 
         <script type="module">
             $(document).ready(function() {
+                //make row become clickable and reroute show user page
+                $(".clickable-row").click(function() {
+                    var href = $(this).data("href");
+                    if (href) {
+                        window.location = href;
+                    }
+                });
+
                 $("#createUser").click(function() {
                     $("#showCreate").removeClass("hidden");
                 });
@@ -168,6 +183,7 @@
                 });
 
                 $(".editUserButton").on("click", function() {
+                    event.stopPropagation(); //prevent other action in parent element (does not execute two actions at the same time)
                     var userId = $(this).data("user-id");
 
                     console.log("Edit Button Clicked for User ID:", userId); // Debugging statement
@@ -181,6 +197,7 @@
 
                             // Assuming the response contains user data as an object
                             // Populate the edit form fields with the user data
+                            $("#showEdit input[name='user_id']").val(userId);
                             $("#showEdit input[name='name']").val(response.name);
                             $("#showEdit input[name='email']").val(response.email);
 
@@ -196,6 +213,46 @@
                 $("#closeFormEdit").click(function() {
                     $("#showEdit").addClass("hidden");
                     $(".showEdit").addClass("hidden");
+                });
+
+                $("#updateUserButton").on("click", function() {
+                    var userId = $("#user_id").val();
+
+                    // Create an object to store the updated user data
+                    var updatedUserData = {
+                        name: $("#showEdit input[name='name']").val(),
+                        email: $("#showEdit input[name='email']").val(),
+                        // Add other fields as needed
+                    };
+
+                    // Use AJAX to send the updated user data to the server
+                    $.ajax({
+                        url: "{{ route('admin.users.update', ['id' => ':id']) }}".replace(':id', userId),
+                        method: "PUT",
+                        data: updatedUserData,
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        success: function(response) {
+
+                            console.log("User updated successfully:", response);
+
+                            // Display the success message
+                            if (response.success) {
+                                var successMessage = response.success;
+
+                                $('#successMessageContainer').html('<div class="flex items-center p-4 mb-4 text-sm bg-green-500 text-green-50 rounded-lg" role="alert"><svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" /></svg><span class="font-medium mr-2">Success alert!</span>' + successMessage + '</div>');
+                            }
+
+                            $("#user-name").text(updatedUserData.name);
+                            $("#user-email").text(updatedUserData.email);
+                            $("#showEdit").addClass("hidden");
+                        },
+                        error: function(error) {
+                            // Handle errors (e.g., display an error message)
+                            console.error("Error updating user:", error);
+                        }
+                    });
                 });
             });
         </script>
